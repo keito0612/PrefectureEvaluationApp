f947b35318d53f286378a68d3355a2b87da035df
isobe keito <iso309532@gmail.com>
2023-08-07 16:12:26 +0900

    Map用のSpotモデルクラスを作成しました。

---
+++ PrefectureEvaluationApp/Model/Spot.swift
@@ -0,0 +1,23 @@
+//
+//  Spot.swift
+//  PrefectureEvaluationApp
+//
+//  Created by 磯部馨仁 on 2023/08/07.
+//
+
+import Foundation
+
+
+import Foundation
+import CoreLocation
+
+struct Spot: Identifiable {
+    let id = UUID()
+    let prefectureName :String
+    let latitude: Double?
+    let longitude: Double?
+    let citys:Array<String>
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+    }
+}
